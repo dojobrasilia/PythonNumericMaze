44b2ff976387025803bffda97ec230e67b178a15
Fabricio Nogueira Buzeto <nuk.anime13@gmail.com>
2012-06-20 18:02:58 -0300

    For now, we solve if the problem is to mutiply by two or split in half forever

--- _NumericMazeTest.py
+++ _NumericMazeTest.py
@@ -3,10 +3,27 @@ from unittest.case import TestCase
 class Walker(object):
     
     def walk(self,source, target):
-        return [source,target]
+        path = [source]
+        
+        while source != target:
+            if( target < source): 
+                source /= 2
+            else:
+                source *= 2
+            path.append(source)
+            
+        return path
 
 class NumericMaze(TestCase):
     
     def test_double_if_the_next_one_is_the_double(self):
         johnny = Walker()
-        self.assertEquals([8,16],johnny.walk(8,16));
+        self.assertEquals([8,16],johnny.walk(8,16))
+            
+    def test_double_if_sequence_of_doubles(self):
+        johnny = Walker()
+        self.assertEquals([4,8,16,32], johnny.walk(4, 32))
+    
+    def test_half_if_sequence_of_halves(self):
+        johnny = Walker()
+        self.assertEquals([32,16,8,4], johnny.walk(32, 4))
